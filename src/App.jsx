41ee1d5605c1d5e5d952960b322437dbc671271b
Leonid Pistryi <61791406+SonOfLion@users.noreply.github.com>
2021-04-06 21:54:01 +0300

    2

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import LoginForm from './LoginForm';
 
 
 const App = () => {
+    const adminUser = {
+        name:'admin',
+        password:'admin'
+    };
+
+    const[user,setUser] = useState({name:'',password:''})
+    const[error,setError] = useState('');
+
+
+    const isLoggedIn = details => {
+        console.log(details)
+    }
+
+    const logout = () => {
+        console.log('logout')
+    }
+
     return (
-        <LoginForm />
+        <form className="app">
+            {(user.name !== '') ? (
+                <div className="welcome">
+                    <h2>Welcome<span>{user.name}</span></h2>
+                    <button >Logout</button>
+                </div>
+            ) : <LoginForm isLoggedIn={isLoggedIn} error={error}/>}
+        </form>
     )
 }
 
--- src/LoginForm.jsx
+++ src/LoginForm.jsx
@@ -1,49 +1,48 @@
-import React,{ useState, useEffect } from 'react';
-import axios from 'axios';
+import React,{ useState } from 'react';
+// import axios from 'axios';
 
-const LoginForm = () => {
-    const[account,setAccount] = useState('');
-    const[password,setPassword] = useState('');
-
-    const handleRequest  = () => {
-        return  axios.get('https://addpi.cloudareus.com:8080/')
-        .then(res => console.log(res))
-    }
-
-
-    const handleChangeAcc = (e) => {
-        setAccount(e.target.account)
-    }
+const LoginForm = ({ isLoggedIn, error }) => {
+    const[description,setDescription] = useState({name: '', password:''})
     
-    const handleChangePass = (e) => {
-        setPassword(e.target.password)
-    }
+    const handleOnSubmit = e => {
+        e.preventDefault();
 
+        isLoggedIn(description);
+    }
     return (
-        <div className="login-form">
-            <input
-                placeholder="Enter youre login"
-                className="login-form__input"
-                type="text"
-                value={account}
-                onChange={handleChangeAcc}
-            >
-            </input>
-            <input
-                placeholder="Enter youre password"
-                className="login-form__input"
-                type="password"
-                value={password}
-                onChange={handleChangePass}
-            >
-            </input>
-            <button 
-                className="login-form__btn"
-                onClick={handleRequest}
-            >Login</button>
-        </div>
+        <form onSubmit={handleOnSubmit}>
+            <div className="form-inner">
+                <h2>Login</h2>
+                <div className="from-group">
+                    <label className="name">Name:</label>
+                    <input type="text" name="name" id="name" onChange={e => setDescription({...description, name:e.target.value})}/>
+                </div>
+                <div className="from-group">
+                    <label htmlFor="password">Password:</label>
+                    <input type="password" name="password" id="password"/>
+                </div>
+                <input type="submit" value="Login"/>
+            </div>
+        </form>
     )
 }
 
-
 export default LoginForm;
+
+{/* <input
+placeholder="Enter youre login"
+className="login-form__input"
+type="text"
+
+>
+</input>
+<input
+placeholder="Enter youre password"
+className="login-form__input"
+type="password"
+>
+</input>
+<button 
+className="login-form__btn"
+
+>Login</button> */}
